3a5e86ea27d8ba5b4ced4682a720609ee4ee68cc
andrewn <andrewnitu@gmail.com>
2017-08-04 01:09:40 -0400

    Add the basic fields and contructor to the runner class

--- easygithuboauth/src/main/java/com/andrewnitu/easygithuboauth/GitHubAuthRunner.java
+++ easygithuboauth/src/main/java/com/andrewnitu/easygithuboauth/GitHubAuthRunner.java
@@ -1,9 +1,27 @@
 package com.andrewnitu.easygithuboauth;
 
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by Andrew Nitu on 8/3/2017.
  */
 
 public class GitHubAuthRunner {
+    private String id;
+    private String secret;
+    private Context context;
 
+    public GitHubAuthRunner(String id, String secret, Context context) {
+        this.id = id;
+        this.secret = secret;
+        this.context = context;
+    }
+
+    public void start() {
+        Intent intent = new Intent(context, AuthActivity.class);
+        intent.putExtra("id", id);
+        intent.putExtra("secret", secret);
+        context.startActivity(intent);
+    }
 }
