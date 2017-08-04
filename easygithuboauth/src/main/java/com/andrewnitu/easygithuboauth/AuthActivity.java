1d4d934a6662cb20333256c075bb14c1b12a8b9a
andrewn <andrewnitu@gmail.com>
2017-08-03 20:29:03 -0400

    Add authentication module skeleton

---
+++ easygithuboauth/src/main/java/com/andrewnitu/easygithuboauth/AuthActivity.java
@@ -0,0 +1,15 @@
+package com.andrewnitu.easygithuboauth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebView;
+
+/**
+ * Created by Andrew Nitu on 8/3/2017.
+ */
+
+public class AuthActivity extends AppCompatActivity {
+
+}
---
+++ easygithuboauth/src/main/java/com/andrewnitu/easygithuboauth/GitHubAuthRunner.java
@@ -0,0 +1,9 @@
+package com.andrewnitu.easygithuboauth;
+
+/**
+ * Created by Andrew Nitu on 8/3/2017.
+ */
+
+public class GitHubAuthRunner {
+
+}
